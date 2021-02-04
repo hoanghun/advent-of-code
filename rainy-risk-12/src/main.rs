840eadba948b7bb67369fa3a578b36b0d9fcd35b
Hung Hoang <hoanghun5@gmail.com>
2021-02-04 22:42:44 +0100

    Rainy risk part two finished.

--- rainy-risk-12/src/main.rs
+++ rainy-risk-12/src/main.rs
@@ -64,6 +64,28 @@ struct Coordinates {
     y: i64,
 }
 
+impl Coordinates {
+    fn rotate(&self, around: &Coordinates, degree: i64) -> Self {
+        let x_distance = self.x - around.x;
+        let y_distance = self.y - around.y;
+        
+        let (rotated_x, rotated_y) = match degree {
+            0 => (x_distance, y_distance),
+            90 => (-y_distance, x_distance),
+            180 => (-x_distance, -y_distance),
+            270 => (y_distance, -x_distance),
+            _ => panic!("Invalid degree!"),
+        };
+
+        Self {
+            x: around.x + rotated_x,
+            y: around.y + rotated_y,
+        }
+
+
+    }
+}
+
 trait Follow {
     fn follow(&mut self, instruction: &Instruction);
 }
@@ -72,6 +94,26 @@ struct Ship {
     coord: Coordinates,
     direction: Direction,
 }
+
+struct WaypointShip {
+    coord: Coordinates,
+    waypoint: Coordinates,
+}
+
+impl WaypointShip {
+    fn new() -> Self {
+        WaypointShip {
+            coord: Coordinates { x: 0, y: 0 },
+            waypoint: Coordinates { x: -10, y: 1},
+        }
+    }
+
+
+    fn get_manhattan_distance_from_start(&self) -> i64 {
+        self.coord.x.abs() + self.coord.y.abs()
+    }
+}
+
 
 impl Ship {
     fn new() -> Self {
@@ -101,12 +143,45 @@ impl Follow for Ship {
     }
 }
 
+
+
+impl Follow for WaypointShip {
+    fn follow(&mut self, instruction: &Instruction) {
+        match instruction {
+            Instruction::North(value) => self.waypoint.y += value,
+            Instruction::West(value) => self.waypoint.x += value,
+            Instruction::South(value) => self.waypoint.y -= value,
+            Instruction::East(value) => self.waypoint.x -= value,
+            Instruction::RotateRight(value) => self.waypoint = self.waypoint.rotate(&self.coord, *value % 360),
+            Instruction::RotateLeft(value) => self.waypoint = self.waypoint.rotate(&self.coord, 360 - (*value % 360)),
+            Instruction::Forward(value) => {
+                let x_distance = (self.waypoint.x - self.coord.x) * value;
+                let y_distance = (self.waypoint.y - self.coord.y) * value;
+
+                self.coord.x += x_distance;
+                self.coord.y += y_distance;
+
+                self.waypoint.x += x_distance;
+                self.waypoint.y += y_distance;
+            }
+        }
+    }
+}
+
 fn part_1(instructions: &Vec<Instruction>) {
     let mut ship = Ship::new();
 
     instructions.iter().for_each(|instr| ship.follow(instr));
 
-    println!("What is the Manhattan distance between that location and the ship's starting position? Answer: {}.", ship.get_manhattan_distance_from_start());
+    println!("Part 1: What is the Manhattan distance between that location and the ship's starting position? Answer: {}.", ship.get_manhattan_distance_from_start());
+}
+
+fn part_2(instructions: &Vec<Instruction>) {
+    let mut ship = WaypointShip::new();
+
+    instructions.iter().for_each(|instr| ship.follow(instr));
+
+    println!("Part 2: What is the Manhattan distance between that location and the ship's starting position? Answer: {}.", ship.get_manhattan_distance_from_start());
 }
 
 fn parse_instruction(line: &str) -> Instruction {
@@ -133,6 +208,7 @@ fn main() -> Result<(), std::io::Error> {
         .collect();
 
     part_1(&instructions);
+    part_2(&instructions);
     Ok(())
 }
 
@@ -165,5 +241,26 @@ mod tests {
         assert_eq!(-17, ship.coord.x);
         assert_eq!(-8, ship.coord.y);
         assert_eq!(25, ship.get_manhattan_distance_from_start());
+    }
+
+    #[test]
+    fn test_part_2() {
+        let mut waypoint_ship = WaypointShip::new();
+        waypoint_ship.follow(&Instruction::Forward(10));
+        assert_eq!((-100, 10), (waypoint_ship.coord.x, waypoint_ship.coord.y));
+
+        waypoint_ship.follow(&Instruction::North(3));
+        assert_eq!((-110, 14), (waypoint_ship.waypoint.x, waypoint_ship.waypoint.y));
+
+        waypoint_ship.follow(&Instruction::Forward(7));
+        assert_eq!((-170, 38), (waypoint_ship.coord.x, waypoint_ship.coord.y));
+        assert_eq!((-180, 42), (waypoint_ship.waypoint.x, waypoint_ship.waypoint.y));
+
+        waypoint_ship.follow(&Instruction::RotateRight(90));
+        assert_eq!((-170, 38), (waypoint_ship.coord.x, waypoint_ship.coord.y));
+        assert_eq!((-174, 28), (waypoint_ship.waypoint.x, waypoint_ship.waypoint.y));
+
+        waypoint_ship.follow(&Instruction::Forward(11));
+        assert_eq!(286, waypoint_ship.get_manhattan_distance_from_start());
     }
 }
